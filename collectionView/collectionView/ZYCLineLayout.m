d72a95d30508fce7301b3aaac5549c822d802630
msxzzyc <10168363742qq.com>
2017-08-01 01:17:33 +0800

    设置拖动后最大cell弹到中间效果
     设置collectionView停止滚动时的偏移量。注意用prepareLayout来做布局的初始化操作（不建议在init中进行），计算最小的间距值的方案，以及最终显示的矩形框需要计算。

--- collectionView/collectionView/ZYCLineLayout.m
+++ collectionView/collectionView/ZYCLineLayout.m
@@ -13,22 +13,35 @@
 {
     if (self = [super init]) {
         
-        /**
-         UICollectionViewLayoutAttributes *attrs;
-         1.一个cell对应一个UICollectionViewLayoutAttributes对象；
-         2.UICollectionViewLayoutAttributes对象决定了cell的frame
-         */
+        
     }
     return self;
 }
 /**
  *当collectionview的显示范围发生改变时，是否需要重写布局
- *一旦重写布局，就会调用layoutAttributesForElementsInRect:方法
+ *一旦重写布局，就会重新调用下面的方法：
+ *1.prepareLayout
+ *2.layoutAttributesForElementsInRect:
  */
 - (BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds
 {
     return YES;
 }
+/**
+ 用来做布局的初始化操作（不建议在init中进行布局的初始化操作）
+ */
+- (void)prepareLayout
+{
+    [super prepareLayout];
+    //设置内边距
+    CGFloat inset = (self.collectionView.frame.size.width - self.itemSize.width)*0.5;
+    self.sectionInset = UIEdgeInsetsMake(0, inset, 0, inset);
+}
+/**
+ UICollectionViewLayoutAttributes *attrs;
+ 1.一个cell对应一个UICollectionViewLayoutAttributes对象；
+ 2.UICollectionViewLayoutAttributes对象决定了cell的frame
+ */
 /**
  *这个方法的返回值是一个数组（数组里面存放着rect范围内所有元素的布局属性）
  *这个方法的返回值决定了rect范围内所有元素的排布（frame）
@@ -53,4 +66,32 @@
 
     return array;
 }
+/**
+ 这个方法的返回值，就决定了collectionView停止滚动时的偏移量
+ */
+- (CGPoint)targetContentOffsetForProposedContentOffset:(CGPoint)proposedContentOffset withScrollingVelocity:(CGPoint)velocity
+{
+    //计算出最终显示的矩形框
+    CGFloat x = proposedContentOffset.x;
+    CGFloat y = 0;
+    CGSize size = self.collectionView.frame.size;
+    CGRect rect = CGRectMake(x, y, size.width, size.height);
+    //获得super已经计算好的布局属性
+    NSArray *array = [self layoutAttributesForElementsInRect:rect];
+    //计算collectionview中心点x
+    CGFloat collectionViewCenterX = proposedContentOffset.x + self.collectionView.frame.size.width*0.5;
+    //存放最小的间距值
+    CGFloat minDelta = MAXFLOAT;
+    for (UICollectionViewLayoutAttributes *attrs in array) {
+        if (ABS(attrs.center.x-collectionViewCenterX)<ABS(minDelta)) {
+            minDelta = attrs.center.x-collectionViewCenterX;
+        
+        }
+        
+    }
+    //修改原有的偏移量
+    proposedContentOffset.x += minDelta;
+    return proposedContentOffset;
+}
+
 @end
